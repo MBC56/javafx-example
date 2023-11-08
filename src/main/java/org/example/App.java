0a04a9dc319af82be39bdb46635363ee157f85a6
MBC56 <martin_bahena@stu.indianhills.edu>
2023-11-07 19:55:50 -0600

    First change

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,11 +9,16 @@ import javafx.event.EventHandler;
 import javafx.geometry.Bounds;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -21,6 +26,8 @@ import javafx.util.Duration;
 /**
  * JavaFX App
  */
+
+
 public class App extends Application {
 
     private final static int SCENE_HEIGHT = 500;
@@ -31,9 +38,18 @@ public class App extends Application {
 
         Pane pane = new Pane();
         Scene scene = new Scene(pane, SCENE_HEIGHT, SCENE_WIDTH);
-        Circle circle = new Circle(20, Color.RED);
-        circle.relocate(10,10);
-        pane.getChildren().add(circle);
+        Rectangle rectangle = new Rectangle(20, 20, Color.RED); // Width, Height, and Color
+        pane.getChildren().add(rectangle);
+
+
+
+        Text bounceText = new Text("Bounce Bounce");
+        bounceText.setFont(Font.font(20));
+        bounceText.setFill(Color.BLACK);
+        bounceText.setLayoutX(130);
+        bounceText.setLayoutY(5);
+
+        pane.getChildren().add(bounceText);
 
         stage.setScene(scene);
         stage.show();
@@ -43,26 +59,28 @@ public class App extends Application {
             double dy = 5;
             @Override
             public void handle(ActionEvent actionEvent) {
-                circle.setLayoutX(circle.getLayoutX() + dx);
-                circle.setLayoutY(circle.getLayoutY() + dy);
+                rectangle.setLayoutX(rectangle.getLayoutX() + dx);
+                rectangle.setLayoutY(rectangle.getLayoutY() + dy);
 
                 Bounds bounds = pane.getBoundsInLocal();
 
-                if(circle.getLayoutX() <= (bounds.getMinX() + circle.getRadius()) ||
-                        circle.getLayoutX() >= (bounds.getMaxX() - circle.getRadius())) {
-                    System.out.println("Out of bounds X " + circle.getLayoutX());
+                if(rectangle.getLayoutX() <= (bounds.getMinX() + rectangle.getWidth()) ||
+                        rectangle.getLayoutX() >= (bounds.getMaxX() - rectangle.getWidth())) {
+                    System.out.println("Out of bounds X " + rectangle.getLayoutX());
 
                     dx = -dx;
                 }
 
-                if(circle.getLayoutY() <= (bounds.getMinY() + circle.getRadius()) ||
-                        circle.getLayoutY() >= (bounds.getMaxY() - circle.getRadius())) {
+                if(rectangle.getLayoutY() <= (bounds.getMinY() + rectangle.getWidth()) ||
+                        rectangle.getLayoutY() >= (bounds.getMaxY() - rectangle.getWidth())) {
                     dy = -dy;
                 }
             }
         }));
         timeline.setCycleCount(Timeline.INDEFINITE);
         timeline.play();
+
+
     }
 
     public static void main(String[] args) {
